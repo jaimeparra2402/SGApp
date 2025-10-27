e893dcb33ef2b753eecf3999f740b75341b29a90
jaimeparra2402 <jpj451@inlumine.ual.es>
2025-10-27 19:30:41 +0100

    Update index.php

--- api/index.php
+++ api/index.php
@@ -28,7 +28,7 @@
       <tbody>
         <?php
 
-        $conexion = mysqli_connect(getenv('MYSQL_HOST'), getenv('MYSQL_USER'), getenv('MYSQL_PASSWORD'), "sg");
+       $conexion = mysqli_connect(getenv('MYSQL_HOST'), getenv('MYSQL_USER'), getenv('MYSQL_PASSWORD'), getenv('MYSQL_DATABASE'));
 
         $cadenaSQL = "select * from s_customer";
         $resultado = mysqli_query($conexion, $cadenaSQL);
